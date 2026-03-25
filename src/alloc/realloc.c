713130c1ebdbc617d54d77b859fc84be493e623c
CrayZiIx <jordannlecomte@gmail.com>
2026-03-25 08:59:14 +0100

    chore: implt. realloc for LARGE zone type only

--- src/alloc/realloc.c
+++ src/alloc/realloc.c
@@ -1,8 +1,34 @@
 #include "malloc.h"
-#include <stddef.h>
+#include <string.h>
 
 void *realloc(void *ptr, size_t size){
-	(void)ptr;
-	(void)size;
-	return (0);
+	t_block *block;
+	void 	*new_ptr;
+	size_t	aligned_size;
+	size_t	copy_size;
+
+	if (ptr == NULL)
+			return(malloc(size));
+	if (size == 0) {
+		free(ptr);
+		return (NULL);
+	}
+
+	block = ((t_block *)ptr - 1);
+	aligned_size = align_size(size, MALLOC_ALIGNMENT);
+
+	// if the existing block is big enough, keep it.
+	if (block->size >= aligned_size)
+		return (ptr);
+
+	new_ptr = malloc(size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (block->size < aligned_size)
+		copy_size = block->size;
+	else
+		copy_size = aligned_size;
+	memcpy(new_ptr, ptr, copy_size);
+	free(ptr);
+	return (new_ptr);
 }
