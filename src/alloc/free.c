4b618f5cf74de75f32a0670b20746aa0f35fb4ed
CrayZiIx <jordannlecomte@gmail.com>
2026-03-25 08:47:37 +0100

    chore: implt. free for LARGE zone type only

--- src/alloc/free.c
+++ src/alloc/free.c
@@ -1,5 +1,20 @@
 #include "malloc.h"
+#include <sys/mman.h>
 
 void free(void *ptr){
-		(void)ptr;
+	t_block *block;
+	t_zone	*zone;
+	if (ptr == NULL)
+		return ;
+	// recover the block from the user pointer
+	block = ((t_block *)ptr) - 1;
+	zone = block->zone;
+	if (zone->prev != NULL)
+		zone->prev->next = zone->next;
+	else
+		g_malloc.zones = zone->next;
+	if (zone->next != NULL)
+		zone->next->prev = zone->prev;
+
+	munmap(zone, zone->size);
 }
