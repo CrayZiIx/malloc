df3f6a444de05a0832a6f152bac5e4ff23dfe343
CrayZiIx <jordannlecomte@gmail.com>
2026-03-26 09:53:53 +0100

    chore: add tests

--- tests/main.c
+++ tests/main.c
@@ -4,19 +4,42 @@
 #include "malloc.h"
 
 int main(void){
-	void *tiny_max;
-	void *small_min;
-	void *small_max;
-	void *large_min;
+	// MIN MAX TEST
+	// void *tiny_max;
+	// void *small_min;
+	// void *small_max;
+	// void *large_min;
 
-	tiny_max = malloc(TINY_MAX);
-	small_min = malloc(TINY_MAX + 1);
-	small_max = malloc(SMALL_MAX);
-	large_min = malloc(SMALL_MAX + 1);
+	// tiny_max = malloc(TINY_MAX);
+	// small_min = malloc(TINY_MAX + 1);
+	// small_max = malloc(SMALL_MAX);
+	// large_min = malloc(SMALL_MAX + 1);
+	// show_alloc_mem();
+	// free(tiny_max);
+	// free(small_min);
+	// free(small_max);
+	// free(large_min);
+
+	void *a;
+	void *b;
+	void *c;
+	void *d;
+	a = malloc(128);
+	b = malloc(128);
+	c = malloc(128);
+	printf("a=%p b=%p c=%p\n", a, b, c);
 	show_alloc_mem();
-	free(tiny_max);
-	free(small_min);
-	free(small_max);
-	free(large_min);
+
+	free(a);
+	free(b);
+	show_alloc_mem();
+
+	d = malloc(256);
+	printf("d=%p\n", d);
+	show_alloc_mem();
+
+	free(c);
+	free(d);
+	show_alloc_mem();
 	return (0);
 }
