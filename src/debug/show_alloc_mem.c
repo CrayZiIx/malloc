b6ad148dc1ca33ac770e1648d19274b5efe7a487
CrayZiIx <jordannlecomte@gmail.com>
2026-03-26 08:41:33 +0100

    chore: enhance show_alloc_mem

--- src/debug/show_alloc_mem.c
+++ src/debug/show_alloc_mem.c
@@ -2,39 +2,54 @@
 #include <stddef.h>
 #include <stdio.h>
 
+
+static size_t print_block(t_block *block);
+static size_t show_zone_type(t_zone_type type, const char *label);
+
 void show_alloc_mem(void){
-	t_zone	*tmp;
-	t_block *block;
-	size_t	total;
-	int		header_flag = 0;
-	void	*start;
-	void	*end;
+	size_t total;
 
-	tmp = g_malloc.zones;
 	total = 0;
-	while (tmp != NULL){
-		block = tmp->blocks;
-		if (!header_flag && tmp->type == ZONE_LARGE) {
-			printf("LARGE : %p\n", (void *)tmp);
-			header_flag = 1;
-		} else if (!header_flag && tmp->type == ZONE_TINY) {
-			printf("TINY : %p\n", (void *)tmp);
-			header_flag = 1;
-		} else if (!header_flag && tmp->type == ZONE_SMALL) {
-			printf("SMALL : %p\n", (void *)tmp);
-			header_flag = 1;
-		}
-		while (block->next != NULL) {
-			if (!block->free) {
-				start = (void *)(block + 1);
-				end = (void *)((char *)(block + 1) + block->size);
-				printf("%p - %p : %zu bytes\n", start, end, block->size);
-				total += block->size;
-			}
-			block = block->next;
-		}
-
-		tmp = tmp->next;
-	}
+	total += show_zone_type(ZONE_TINY, "TINY");
+	total += show_zone_type(ZONE_SMALL, "SMALL");
+	total += show_zone_type(ZONE_LARGE, "LARGE");
 	printf("Total : %zu bytes\n", total);
+}
+
+static size_t print_block(t_block *block) {
+	void *start;
+	void *end;
+
+	if (block->free)
+		return (0);
+	start = (void *)(block + 1);
+	end = (void *)((char * )(block + 1) + block->size);
+	printf("%p - %p : %zu bytes\n", start, end, block->size);
+	return (block->size);
+}
+
+static size_t show_zone_type(t_zone_type type, const char *label) {
+	t_zone	*zone;
+	t_block	*block;
+	size_t	subtotal;
+	int		printed_header;
+
+	zone = g_malloc.zones;
+	subtotal = 0;
+	printed_header = 0;
+	while (zone != NULL) {
+		if (zone->type == type) {
+			if (!printed_header) {
+				printf("%s : %p\n", label, (void *)zone);
+				printed_header = 1;
+			}
+			block = zone->blocks;
+			while (block != NULL) {
+				subtotal += print_block(block);
+				block = block->next;
+			}
+		}
+		zone = zone->next;
+	}
+	return (subtotal);
 }
